a98348df633221a8b609debdcc58a639bce3f7ff
Hugo Tamayo <doctamayot@hotmail.com>
2023-11-21 09:13:47 -0500

    demo2

--- app/user/administrar-usuarios/agregar-agente-de-servicio/page.jsx
+++ app/user/administrar-usuarios/agregar-agente-de-servicio/page.jsx
@@ -2,16 +2,7 @@
 import { CreateServiceAgent } from "@/components";
 
 const ServiceAgentRegister = () => {
-  return (
-  
-    <div className='h-screen w-screen p-8 ml-8'>
-          <div className="avant-garde-bold font-bold text-3xl  text-black">
-            <h2 className="text-xl ml-16">Agregar Agente de Servicio</h2>
-          </div>
-          
-          <CreateServiceAgent />
-        </div>
-  );
+  return <CreateServiceAgent />;
 };
 
 export default ServiceAgentRegister;
